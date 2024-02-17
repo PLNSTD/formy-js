85df0d38e69986bb3d7219b6ef01a6f5db0f336f
Plidher <65611683+PLNSTD@users.noreply.github.com>
2024-02-17 01:17:50 -0500

    Everything is done!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import './style.css';
 // import icon from './icon.png'; To load some image for the code
 
-for (let i = 0; i < 3; i += 1) {
-  console.log(i);
+function main() {
+  const formElement = document.getElementById('form');
+  const submitBtn = document.getElementById('submit-btn');
+
+  formElement.addEventListener('submit', () => {
+    const correctFormText = document.createElement('p');
+    correctFormText.classList.add('correct-message');
+    correctFormText.innerHTML = "Form has been submitted correctly, Thanks!";
+    formElement.innerHTML = "";
+
+    formElement.appendChild(correctFormText);
+  })
+
+  submitBtn.addEventListener('click', () => {
+    formElement.classList.add('submitted');
+  })
 }
+
+window.addEventListener('load', main);
